3e5c686dd9bab3a38ab026b4af7b4b859b2f8fef
Zowrer <zourer@gmail.com>
2015-11-17 12:47:07 -0600

    modificacion para el giroscopio y landscape para el dispositivo

--- js/script.js
+++ js/script.js
@@ -1,47 +1,44 @@
 var box = '#ball', drag = $('.ball'), drop = $('.key');
 
+var x=0;
+var y=0;
+
+var x1=0;
+var y2=0;
+
 var ball   = document.querySelector('#ball');
 var garden = document.querySelector('.keyboard');
 
 var maxX = garden.clientWidth  - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
 
-var x = 0;
-var y = 0;
-
 function handleOrientation(event) {
-  //var x = event.beta;
-  //var y = event.gamma;
 
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-  //if (x >  90) { x =  90};
-  //if (x < -90) { x = -90};
+  if ((event.beta >  x1) && ( x< 300 )) { x+=  5}
+  else
+	if (x >50)
+		{ x -=5};
+
+  if ((event.gamma >  y2) && (y< 800)) { y+=  5}
+  else
+	if (y > 50)
+	{ y -= 5};
 
   // To make computation easier we shift the range of
-  // x and y to [0,180]
-  //x += 90;
-  //y += 90;
-
-  if (event.beta > 0) {
-      x += 1;
-  } else {
-      x -= 1;
-  }
-
-  if (event.gamma > 0) {
-      y += 1;
-  } else {
-      y -= 1;
-  }
+  // // x and y to [0,180]
+  // x += 90;
+  // y += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  //ball.style.top  = (maxX*x/180 - 10) + "px";
-  //ball.style.left = (maxY*y/180 - 10) + "px";
-  ball.style.top = x + "px";
+  ball.style.top  = x + "px";
   ball.style.left = y + "px";
+
+   x1 = event.beta;
+   y2 = event.gamma;
 
   /*** :plugin specific code ***/
 
